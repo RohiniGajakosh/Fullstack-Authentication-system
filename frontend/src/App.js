a1fe9b6b0c23bb81fb4287f6dc47c3eae9c247f3
RohiniGajakosh <rohinigajakosh01@gmail.com>
2025-12-19 10:54:51 +0530

    added default route in app.js frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <nav>
         <Link to="/login">Login</Link> |{" "}
         <Link to="/register">Register</Link>
       </nav>
 
       <Routes>
+        {/* Default route */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
